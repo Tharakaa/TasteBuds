9e07e248da607a0f99c8faa98966976491f0ebf1
Nadunika <nadunikaperera1@gmail.com>
2022-10-08 16:06:50 +0530

    get items by outlet client implement

--- client/src/components/client/ItemCard.js
+++ client/src/components/client/ItemCard.js
@@ -1,13 +1,52 @@
-import { Link } from "react-router-dom";
+import React from "react";
+import Rating from "@mui/material/Rating";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 
-const ItemCard = () => {
+const ItemCard = ({ item }) => {
   return (
-    <>
-      <div>Item card</div>
-      <button>
-        <Link to="/outlet/1">View Item</Link>
-      </button>
-    </>
+    <React.Fragment>
+      <div
+        className="card mb-4 shadow bg-white rounded"
+        style={{ backgroundColor: "white" }}
+      >
+        <h2
+          className="text-center p-1"
+          style={{ color: "black", fontWeight: "bolder" }}
+        >
+          {item.name}
+        </h2>
+        <div className="overflow-hidden " style={{}}>
+          <img src={item.imgUrl} className="card-img-top zoom" alt="..." />
+        </div>
+
+        <div className="card-body ">
+          <div className="d-flex justify-content-between ">
+            <div className="d-flex ">
+              <h5>{item.rating.toFixed(1)} &nbsp; </h5>
+              <Rating
+                name="half-rating-read"
+                defaultValue={item.rating}
+                precision={0.5}
+                readOnly
+              />
+            </div>
+            <div>
+              <FavoriteIcon />
+              <FavoriteBorderIcon />
+            </div>
+          </div>
+          <h4>Rs.{item.price}</h4>
+          <h5>Description: {item.description}</h5>
+
+          <button className="btn btn-primary">
+            Add to cart &nbsp;
+            <ShoppingCartIcon />
+          </button>
+        </div>
+      </div>
+    </React.Fragment>
   );
 };
 
