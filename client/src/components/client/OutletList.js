1bc8e7ff7ac35c0401a6c7bdf7cb777f71860f70
Nadunika <nadunikaperera1@gmail.com>
2022-10-08 15:47:13 +0530

    get all outlet client implement

--- client/src/components/client/OutletList.js
+++ client/src/components/client/OutletList.js
@@ -1,14 +1,39 @@
 import OutletCard from "./OutletCard";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const OutletList = () => {
+  const [outletArr, setOutletArr] = useState([]);
+
+  const getAllOutlet = async () => {
+    const { data } = await axios.get("http://localhost:5000/api/outlets"); 
+    setOutletArr(data);
+  };
+  
+  useEffect(() => {
+    getAllOutlet();
+  }, []); 
+
   return (
-    <>
-      <div className="row">
-        <div className="col-12 col-md-6 col-lg-4 col-xl-3">
-          <OutletCard />
-        </div>
+    <React.Fragment>
+      <div className="container wrapper">
+        <h1
+          className="text-center p-5 text-black "
+          style={{ fontWeight: "bolder" }}
+        >
+          Famous Food Outlets
+        </h1>
       </div>
-    </>
+      <div className="row">
+        {outletArr.map((outlet) => {
+          return (
+            <div className="col-12 col-md-6 col-lg-3 " key={outlet._id}>
+              <OutletCard outlet={outlet} />
+            </div>
+          );
+        })}
+      </div>
+    </React.Fragment>
   );
 };
 
