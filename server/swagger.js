2ec3ae7a8729321f9f14467e26b0a0003bcf1615
Tharaka-T-Gamachchi <86717804+Tharaka-T-Gamachchi@users.noreply.github.com>
2022-10-12 21:11:19 +0530

    Merge branch 'tharaka' into tharaka

--- server/swagger.js
+++ server/swagger.js
@@ -5,12 +5,12 @@ const doc = {
         title: 'My API',
         description: 'Description',
     },
-    host: 'localhost:3000',
+    host: 'localhost:8080',
     schemes: ['http'],
 };
 
 const outputFile = 'swagger-output.json';
-const endpointsFiles = ['./src/routers/cart.js', './src/routers/order.js', './src/routers/item.js', './src/routers/outlet.js', './src/routers/user.js', './src/routers/util.js', './src/routers/wishlist.js'];
+const endpointsFiles = ['./src/app.js'];
 
 /* NOTE: if you use the express Router, you must pass in the
    'endpointsFiles' only the root file where the route starts,
