1defd5e446780cc01b9fa41d31ca1bcff532dc2c
Tharaka-T-Gamachchi <86717804+Tharaka-T-Gamachchi@users.noreply.github.com>
2022-10-12 20:31:12 +0530

    Swagger-output

--- server/swagger.js
+++ server/swagger.js
@@ -10,7 +10,7 @@ const doc = {
 };
 
 const outputFile = 'swagger-output.json';
-const endpointsFiles = ['./src/routers/cart.js', './src/routers/order.js', './src/routers/item.js', './src/routers/outlet.js', './src/routers/user.js'];
+const endpointsFiles = ['./src/routers/cart.js', './src/routers/order.js', './src/routers/item.js', './src/routers/outlet.js', './src/routers/user.js', './src/routers/util.js', './src/routers/wishlist.js'];
 
 /* NOTE: if you use the express Router, you must pass in the
    'endpointsFiles' only the root file where the route starts,
